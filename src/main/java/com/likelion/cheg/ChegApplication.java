341fcfec84cd45f1247c7563ed18c8108112c9c7
kangdukyoung <kang48450@gmail.com>
2023-03-29 18:46:29 +0900

    Chore: add war packaging in pom.xml

--- src/main/java/com/likelion/cheg/ChegApplication.java
+++ src/main/java/com/likelion/cheg/ChegApplication.java
@@ -4,10 +4,11 @@ import com.likelion.cheg.domain.category.CategoryRepository;
 import org.hibernate.sql.Insert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ChegApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChegApplication.class, args);
 	}
