01eb9e489a122249ac32aba8820b301e72aa645e
kangdukyoung <kang48450@gmail.com>
2023-05-28 13:03:18 +0900

    Chore: swagger-test

---
+++ src/main/java/com/likelion/cheg/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.likelion.cheg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    // http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.likelion.cheg"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API 문서")
+                .description("API에 대해서 설명해주는 문서입니다.")
+                .version("3.0")
+                .build();
+    }
+
+}
--- src/main/java/com/likelion/cheg/web/apiController/UserApiController.java
+++ src/main/java/com/likelion/cheg/web/apiController/UserApiController.java
@@ -5,6 +5,9 @@ import com.likelion.cheg.domain.user.User;
 import com.likelion.cheg.service.UserService;
 import com.likelion.cheg.web.dto.CMResponse;
 import com.likelion.cheg.web.dto.user.UserUpdateDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "user controller",description = "유저관련api")
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
@@ -24,6 +28,8 @@ public class UserApiController {
         return new ResponseEntity<>(new CMResponse<>(1,"회원 탈퇴 성공",""),HttpStatus.OK);
     }
 
+
+    @ApiOperation(value="회원 수정", notes = "회원을 수정한다.")
     @PutMapping("api/users/{userId}")
     public ResponseEntity<CMResponse> updateUser(
             @PathVariable int userId,
